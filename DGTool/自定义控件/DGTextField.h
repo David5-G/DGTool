474e0e3131a4671b59411c9eed11e09255246d8e
david <david@david-mini.local>
2019-11-21 17:33:52 +0800

    优化DGTextField

--- DGTool/自定义控件/DGTextField.h
+++ DGTool/自定义控件/DGTextField.h
@@ -2,8 +2,8 @@
 //  DGTextField.h
 //  DGTool
 //
-//  Created by jczj on 2018/11/2.
-//  Copyright © 2018年 david. All rights reserved.
+//  Created by David.G on 16/7/20.
+//  Copyright © 2016年 david. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,14 +12,28 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DGTextField : UITextField
 
-//上移后，textField需要额外高于键盘顶部的距离，默认为0
-@property (nonatomic, assign) CGFloat offset;
+/** 上移后，textField需要额外高于键盘顶部的距离，默认为0 */
+@property (nonatomic, assign) CGFloat spaceY;
 
-//需要向上移动的view，默认为keyWindow
+/** 需要向上移动的view，默认为keyWindow */
 @property (nonatomic, weak) UIView *movingView;
 
+/**
+ * ① 默认不设置,会在willShowKeyboard中自动获取movingView的frame
+ * ② 设置, 就不会再自动获取
+ */
+@property (nonatomic, assign) CGRect movingViewOriginalFrame;
 
--(instancetype)initWithFrame:(CGRect)frame textFont:(CGFloat)font  textColor:(UIColor *)color bagTextColor:(UIColor *)bagColor  textAlignment:(NSTextAlignment)alignment;
+/** 设置字体,textColor, agignment */
+-(void)setFont:(CGFloat)font textColor:(UIColor *)color textAlignment:(NSTextAlignment)alignment;
+
+/** 设置InputAccessoryView, 用于点击"完成"撤销键盘 */
+-(void)setDoneInputAccessoryView;
+
+
+#pragma mark - range
+@property (nonatomic, assign) NSRange selectedRange;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- DGTool/自定义控件/DGTextField.m
+++ DGTool/自定义控件/DGTextField.m
@@ -2,28 +2,19 @@
 //  DGTextField.m
 //  DGTool
 //
-//  Created by jczj on 2018/11/2.
-//  Copyright © 2018年 david. All rights reserved.
+//  Created by David.G on 16/7/20.
+//  Copyright © 2016年 david. All rights reserved.
 //
 
 #import "DGTextField.h"
 
 @interface DGTextField ()
 
-@property (nonatomic, assign) CGRect originalFrame;
-
 @end
 
 @implementation DGTextField
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
+#pragma mark - life circle
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
@@ -32,27 +23,7 @@
     return self;
 }
 
--(instancetype)initWithFrame:(CGRect)frame textFont:(CGFloat)font  textColor:(UIColor *)color bagTextColor:(UIColor *)bagColor  textAlignment:(NSTextAlignment)alignment
-{
-    
-    self = [super initWithFrame:frame];
-    if (self) {
-//        [self onInit];
-        self.autocapitalizationType = UITextAutocapitalizationTypeNone;
-        self.autocorrectionType = UITextAutocorrectionTypeNo;
-        [self setTextColor:color];
-        [self setValue:bagColor forKeyPath:@"_placeholderLabel.textColor"];
-        self.clearButtonMode = UITextFieldViewModeWhileEditing;
-        self.textAlignment = alignment;
-        self.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
-        self.keyboardType = UIKeyboardTypeDefault;
-        self.font = [UIFont systemFontOfSize:font];
-        self.returnKeyType = UIReturnKeyDone;
-    }
-    
-    return self;
-    
-}
+
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
@@ -63,54 +34,140 @@
 }
 
 - (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+    [self removeKeyBoardNotifications];
 }
+
+#pragma mark - config
 
 - (void)onInit {
     [self addKeyboardNotifications];
-    _movingView = [UIApplication sharedApplication].keyWindow;
-    _originalFrame = CGRectZero;
+    self.movingView = [UIApplication sharedApplication].keyWindow;
+    self.movingViewOriginalFrame = CGRectZero;
 }
 
+-(void)setFont:(CGFloat)font textColor:(UIColor *)color textAlignment:(NSTextAlignment)alignment {
+
+    [self setTextColor:color];
+    self.textAlignment = alignment;
+    self.font = [UIFont systemFontOfSize:font];
+    self.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    //self.autocorrectionType = UITextAutocorrectionTypeNo;
+    //self.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    self.keyboardType = UIKeyboardTypeDefault;
+    self.returnKeyType = UIReturnKeyDone;
+}
+
+/** 设置InputAccessoryView, 用于点击"完成"撤销键盘 */
+-(void)setDoneInputAccessoryView {
+    
+    //1.btn
+    UIButton *doneBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 40, 30)];
+    [doneBtn setTitle:@"完成" forState:UIControlStateNormal];
+    [doneBtn setTitleColor:self.tintColor forState:UIControlStateNormal];
+    [doneBtn addTarget:self action:@selector(closeKeyboard) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *btnItem = [[UIBarButtonItem alloc] initWithCustomView:doneBtn];
+    
+    //2.fixItem
+    UIBarButtonItem *fixItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:self action:nil];
+    fixItem.width = 1;
+    
+    //3.spaceItem
+    UIBarButtonItem *flexibleItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    
+    //4.toolBar
+    CGFloat toolBarH = 44;
+    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth([UIScreen mainScreen].bounds), toolBarH)];
+    toolBar.items = @[flexibleItem, btnItem, fixItem];
+    self.inputAccessoryView = toolBar;
+}
+
+-(void)closeKeyboard {
+    [self resignFirstResponder];
+}
+
+#pragma mark - keyboard notification
 - (void)addKeyboardNotifications {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
 
+- (void)removeKeyBoardNotifications {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
 - (void)keyboardWillShow: (NSNotification *)notification {
-    if (self.isFirstResponder) {
-        CGPoint relativePoint = [self convertPoint: CGPointZero toView: [UIApplication sharedApplication].keyWindow];
-        
-        CGFloat keyboardHeight = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
-        CGFloat overstep = CGRectGetHeight(self.frame) + relativePoint.y + keyboardHeight - CGRectGetHeight([UIScreen mainScreen].bounds);
-        overstep += self.offset;
-        
-        if (CGRectEqualToRect(self.originalFrame, CGRectZero)) {
-            self.originalFrame = self.movingView.frame;
-        }
-        
-        if (overstep > 0) {
-            CGFloat duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-            CGRect frame = self.originalFrame;
-            frame.origin.y -= overstep;
-            [UIView animateWithDuration: duration delay: 0 options: UIViewAnimationOptionCurveLinear animations: ^{
-                self.movingView.frame = frame;
-            } completion: nil];
-        }
-    }
+    
+    //1.过滤self非第一响应者
+    if (!self.isFirstResponder) {  return ; }
+    
+    //2.计算
+    //键盘高度
+    CGFloat keyboardHeight = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    //self的相对keyWindow的origin
+    CGPoint relativePoint = [self convertPoint: CGPointZero toView: [UIApplication sharedApplication].keyWindow];
+    //要移动的距离 (移动多少才能展示self)
+    CGFloat overstep = CGRectGetHeight(self.frame) + relativePoint.y + keyboardHeight - CGRectGetHeight([UIScreen mainScreen].bounds);
+    //加上偏移高度
+    overstep += self.spaceY;
+    
+    //在这里要记录一下当前movingView的frame, 用于keyboardWillHide中将movingView的frame复位
+    self.movingViewOriginalFrame = self.movingView.frame;
+    
+    //3. 如果overstep<=0代表不被遮挡,不需要移动
+    if (overstep < 0) {  return ; }
+    
+    //4.动画
+    CGFloat duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect frame = self.movingView.frame;
+    frame.origin.y -= overstep;
+    [UIView animateWithDuration: duration delay: 0 options: UIViewAnimationOptionCurveLinear animations: ^{
+        self.movingView.frame = frame;
+    } completion: nil];
+
 }
 
-- (void)keyboardWillHide: (NSNotification *)notification {
-    if (self.isFirstResponder) {
-        CGFloat duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-        [UIView animateWithDuration: duration delay: 0 options: UIViewAnimationOptionCurveLinear animations: ^{
-            self.movingView.frame = self.originalFrame;
-        } completion: nil];
-        self.originalFrame = CGRectZero;
-    }
+- (void)keyboardWillHide:(NSNotification *)notification {
+    //1.过滤self非第一响应者
+    if (!self.isFirstResponder) { return ;}
+    
+    //2.动画
+    CGFloat duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    [UIView animateWithDuration: duration delay: 0 options: UIViewAnimationOptionCurveLinear animations: ^{
+        self.movingView.frame = self.movingViewOriginalFrame;
+        
+    } completion:^(BOOL finished) {
+        self.movingViewOriginalFrame = self.movingView.frame;
+    }];
+    
 }
 
 
+#pragma mark - range
+- (NSRange) selectedRange {
+    UITextPosition* beginning = self.beginningOfDocument;
+    
+    UITextRange* selectedRange = self.selectedTextRange;
+    UITextPosition* selectionStart = selectedRange.start;
+    UITextPosition* selectionEnd = selectedRange.end;
+    
+    const NSInteger location = [self offsetFromPosition:beginning toPosition:selectionStart];
+    const NSInteger length = [self offsetFromPosition:selectionStart toPosition:selectionEnd];
+    
+    return NSMakeRange(location, length);
+}
 
+// 备注：UITextField必须为第一响应者才有效
+- (void) setSelectedRange:(NSRange) range {
+    
+    UITextPosition* beginning = self.beginningOfDocument;
+    
+    UITextPosition* startPosition = [self positionFromPosition:beginning offset:range.location];
+    UITextPosition* endPosition = [self positionFromPosition:beginning offset:range.location + range.length];
+    UITextRange* selectionRange = [self textRangeFromPosition:startPosition toPosition:endPosition];
+    
+    [self setSelectedTextRange:selectionRange];
+}
+ 
 @end
